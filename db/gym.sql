cfcb96ab460069430e04ce97fc7d188fe90ecd7b
EmilZacharczuk <emil.zacharczuk@yahoo.pl>
2019-02-08 22:01:11 +0000

    Added db, tables and sql_runner

---
+++ db/gym.sql
@@ -0,0 +1,24 @@
+DROP TABLE bookings;
+DROP TABLE sessions;
+DROP TABLE members;
+
+CREATE TABLE members (
+  id SERIAL4 PRIMARY KEY,
+  first_name VARCHAR(225),
+  last_name VARCHAR(225),
+  address VARCHAR(225)
+);
+
+CREATE TABLE sessions (
+  id SERIAL4 PRIMARY KEY,
+  title VARCHAR(225),
+  instructor VARCHAR(225),
+  day VARCHAR(225),
+  start_time TIMESTAMP
+);
+
+CREATE TABLE bookings (
+  id SERIAL4 PRIMARY KEY,
+  member_id INT4 REFERENCES members(id) ON DELETE CASCADE,
+  session_id INT4 REFERENCES sessions(id) ON DELETE CASCADE
+);
