0fe2f5e14b75cfa91ed16122bfd987fdf6c3b14e
EmilZacharczuk <emil.zacharczuk@yahoo.pl>
2019-02-09 16:18:27 +0000

    Member class tested

--- models/specs/member_spec.rb
+++ models/specs/member_spec.rb
@@ -12,6 +12,23 @@ class TestMember < Minitest::Test
     @member = Member.new(options)
   end
 
+  def test_id()
+    assert_equal(1, @member.id)
+  end
 
+  def test_firsr_name()
+    assert_equal("John", @member.first_name)
+  end
 
+  def test_second_name()
+    assert_equal("Walker", @member.second_name)
+  end
+
+  def test_address()
+    assert_equal("26 Buchanan Street EH3 5BF Edinburgh", @member.address)
+  end
+
+  def test_pretty_name()
+    assert_equal("John Walker", @member.pretty_name)
+  end
 end
