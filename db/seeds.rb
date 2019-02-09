83cf73b97cb2cee949c23524ffe3b893675f764a
EmilZacharczuk <emil.zacharczuk@yahoo.pl>
2019-02-09 21:53:14 +0000

    CRUD for member added and tested

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ require_relative('../models/session')
 require_relative('../models/booking')
 require("pry-byebug")
 
-
+Member.delete_all
 
 member_1 = Member.new(
   {"first_name" => "John",
@@ -11,33 +11,41 @@ member_1 = Member.new(
    "address" => "26 Buchanan Street EH3 5BF Edinburgh"
   })
 
+member_1.save()
+
 member_2 = Member.new(
   {"first_name" => "Andrew",
    "second_name" => "Wilson",
    "address" => "54 Frederick Street EH1 8BF Edinburgh"
     })
+
+member_2.save()
+
 member_3 = Member.new(
   {"first_name" => "Phil",
    "second_name" => "Collins",
    "address" => "3A Leith Walk EH5 9XF Edinburgh"
     })
+
+member_3.save()
+
 member_4 = Member.new(
     {"first_name" => "Steve",
      "second_name" => "Harrison",
      "address" => "17/4 Woodburn Terrace EH22 2FT Dalkeith"
       })
+
+member_4.save()
+
 member_5 = Member.new(
     {"first_name" => "Peter",
      "second_name" => "McDonald",
      "address" => "6 Rose Avenue EH17 5GF Broxburn"
       })
 
-member_1.save()
-member_2.save()
-member_3.save()
-member_4.save()
 member_5.save()
-member_1.first_name = 'Jack'
-member_1.update()
+
+
+
 binding.pry
 nil
