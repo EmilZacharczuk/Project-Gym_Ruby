5430e721b3f393dceb7c843e9c609b74a9763bb7
EmilZacharczuk <emil.zacharczuk@yahoo.pl>
2019-02-09 16:23:51 +0000

    Class booking tested

--- models/specs/booking_spec.rb
+++ models/specs/booking_spec.rb
@@ -6,11 +6,20 @@ class TestBooking < Minitest::Test
   def setup
     options = {"id" => 1,
               "member_id" => 1,
-              "lesson_id" => 2}
-              
+              "session_id" => 2}
+
     @booking = Booking.new(options)
   end
 
+  def test_id
+    assert_equal(1, @booking.id)
+  end
 
+  def test_member_id
+    assert_equal(1, @booking.member_id)
+  end
 
+  def test_session_id
+    assert_equal(2, @booking.session_id)
+  end
 end
