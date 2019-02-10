97042cc5a4b22e1136b27603c1853a43a86cdb56
EmilZacharczuk <emil.zacharczuk@yahoo.pl>
2019-02-10 14:20:07 +0000

    Added sessions function to Member and tested

--- db/gym.sql
+++ db/gym.sql
@@ -3,14 +3,14 @@ DROP TABLE sessions;
 DROP TABLE members;
 
 CREATE TABLE members (
-  id SERIAL4 PRIMARY KEY,
+  id SERIAL8 PRIMARY KEY,
   first_name VARCHAR(225),
   second_name VARCHAR(225),
   address VARCHAR(225)
 );
 
 CREATE TABLE sessions (
-  id SERIAL4 PRIMARY KEY,
+  id SERIAL8 PRIMARY KEY,
   title VARCHAR(225),
   instructor VARCHAR(225),
   day VARCHAR(225),
@@ -18,7 +18,7 @@ CREATE TABLE sessions (
 );
 
 CREATE TABLE bookings (
-  id SERIAL4 PRIMARY KEY,
-  member_id INT4 REFERENCES members(id) ON DELETE CASCADE,
-  session_id INT4 REFERENCES sessions(id) ON DELETE CASCADE
+  id SERIAL8 PRIMARY KEY,
+  member_id INT8 REFERENCES members(id) ON DELETE CASCADE,
+  session_id INT8 REFERENCES sessions(id) ON DELETE CASCADE
 );
