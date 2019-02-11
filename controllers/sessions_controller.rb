706c0b7478bb6b7154abe27bdbcedd8d3274f4cc
EmilZacharczuk <emil.zacharczuk@yahoo.pl>
2019-02-11 14:58:46 +0000

    New view and new post added to sessions

--- controllers/sessions_controller.rb
+++ controllers/sessions_controller.rb
@@ -8,7 +8,16 @@ get '/sessions' do
   erb (:"sessions/index")
 end
 
+get '/sessions/new' do
+  erb (:"sessions/new")
+end
+
 get '/sessions/:id' do
   @session = Session.find(params['id'].to_i)
   erb (:"sessions/show")
+end
+
+post '/sessions' do
+  Session.new(params).save
+  redirect to '/sessions'
 end
