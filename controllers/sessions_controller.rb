2cb927fb54e660f6ab0934969f806a8ffffdb353
EmilZacharczuk <emil.zacharczuk@yahoo.pl>
2019-02-11 11:20:42 +0000

    Index view for sessions

--- controllers/sessions_controller.rb
+++ controllers/sessions_controller.rb
@@ -2,3 +2,8 @@ require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require_relative('../models/session.rb')
 also_reload('../models/*')
+
+get '/sessions' do
+  @sessions = Session.all()
+  erb (:"sessions/index")
+end
