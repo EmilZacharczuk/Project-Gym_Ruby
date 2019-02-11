465c275724662489d4b57d47de7b616e51785e81
EmilZacharczuk <emil.zacharczuk@yahoo.pl>
2019-02-11 14:42:46 +0000

    New view and new post added to members

--- controllers/members_controller.rb
+++ controllers/members_controller.rb
@@ -8,7 +8,16 @@ get '/members' do
   erb (:"members/index")
 end
 
+get '/members/new' do
+  erb (:"members/new")
+end
+
 get '/members/:id' do
   @member = Member.find(params['id'].to_i)
   erb (:"members/show")
+end
+
+post '/members' do
+  Member.new(params).save
+  redirect to '/members'
 end
