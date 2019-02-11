94179da2ef4378d6cb6788a4438e31eec0161a33
EmilZacharczuk <emil.zacharczuk@yahoo.pl>
2019-02-11 11:55:22 +0000

    Index view for bookings

--- controllers/bookings_controller.rb
+++ controllers/bookings_controller.rb
@@ -1,4 +1,11 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require_relative('../models/booking.rb')
+require_relative('../models/member.rb')
+require_relative('../models/session.rb')
 also_reload('../models/*')
+
+get '/bookings' do
+  @bookings = Booking.all()
+  erb (:"bookings/index")
+end
